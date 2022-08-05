7f763dcda6519f2e1681900e90d0ab82a463bd0f
Kiran <jinkachandrakiran2002@gmail.com>
2022-08-05 22:32:06 +0530

    saca

---
+++ scripts/mail.js
@@ -0,0 +1,36 @@
+function sendMail(){
+    var name=document.getElementById("name").value
+    var email=document.getElementById("email").value
+    var body=document.getElementById("message").value
+    console.log(name+"  "+email+"  "+body)
+
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+    "email": email,
+    "name": name,
+    "body": body
+    });
+
+    var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+    };
+
+    fetch("https://backendresume.herokuapp.com/login", requestOptions)
+    .then(response => response.text())
+    .then(result => {
+        if(result=="success"){
+            alert("Message sent successfuly")
+        }
+        else{
+            alert("Please check your mail id")
+        }
+    })
+    .catch(error => console.log('error', error));
+}
+
